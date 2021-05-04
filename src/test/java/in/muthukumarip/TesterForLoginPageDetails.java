2516c0382823a8f22faa0eac48a914f1f1b0b930
MuthuKumari-P <82449715+MuthuKumari-P@users.noreply.github.com>
2021-05-04 14:58:04 +0530

    Update TesterForLoginPageDetails.java

--- src/test/java/in/muthukumarip/TesterForLoginPageDetails.java
+++ src/test/java/in/muthukumarip/TesterForLoginPageDetails.java
@@ -2,8 +2,6 @@ package in.muthukumarip;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 public class TesterForLoginPageDetails {
@@ -11,31 +9,20 @@ public class TesterForLoginPageDetails {
 	// Testcase for correct input
 	@Test
 	public void testCase1() {
-		long mobileNumber = 9767456764L;
-		int passWord = 4367;
-		ArrayList<String> errors = CheckLoginPageDetails.validateLoginPagedDetails(mobileNumber, passWord);
-		if (errors.isEmpty()) {
-			System.out.println("Login Details are valid");
-		} else {
-			System.out.println("Errors\n" + errors);
-
-		}
-		assertEquals(true, errors.isEmpty());
+		long mobileNumber = 8344793922L;
+		String password = "muthu@99P";
+		boolean valid = CheckLoginPageDetails.checkLoginDetails(mobileNumber, password);
+		assertEquals(true, valid);
 	}
 
 	// Testcase for incorrect input
 	@Test
 	public void testCase2() {
 		long mobileNumber = 9767677764L;
-		int passWord = 437;
-		ArrayList<String> errors = CheckLoginPageDetails.validateLoginPagedDetails(mobileNumber, passWord);
-		if (errors.isEmpty()) {
-			System.out.println("Login Details are valid");
-		} else {
-			System.out.println("Errors\n" + errors);
+		String password ="437";
+		boolean valid = CheckLoginPageDetails.checkLoginDetails(mobileNumber, password);
+		assertEquals(false, valid);
 
-		}
-		assertEquals(false, errors.isEmpty());
 	}
 
 }
