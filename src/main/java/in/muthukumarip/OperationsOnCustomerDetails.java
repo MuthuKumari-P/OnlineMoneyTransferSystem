0572598471c51d2770e21f0465ccbd00f648a037
MuthuKumari-P <82449715+MuthuKumari-P@users.noreply.github.com>
2021-05-04 14:51:16 +0530

    Update OperationsOnCustomerDetails.java

--- src/main/java/in/muthukumarip/OperationsOnCustomerDetails.java
+++ src/main/java/in/muthukumarip/OperationsOnCustomerDetails.java
@@ -3,18 +3,18 @@ package in.muthukumarip;
 import java.util.ArrayList;
 
 public class OperationsOnCustomerDetails {
-	static ArrayList<UserDetails> customer = new ArrayList<UserDetails>();
+	static ArrayList<UserDetails> customerDetails = new ArrayList<UserDetails>();
 
 	// This method used to add the customer details
 	public static void addCustomerDetails(UserDetails user) {
-		customer.add(user);
+		customerDetails.add(user);
 	}
 
 	// This method used to display the customer bank details
 	public static void displayCustomerDetails() {
-		for (UserDetails customerDetails : customer) {
-			System.out.println(customerDetails);
-			System.out.println("-----------------------------------");
+		for (UserDetails customerDetail : customerDetails) {
+			System.out.println(customerDetail);
+			System.out.println("-----------------*****-----------------");
 		}
 	}
 }
