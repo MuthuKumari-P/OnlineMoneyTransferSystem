0cb320639de921eb2caa4c28d776b647dfc3937c
MuthuKumari-P <82449715+MuthuKumari-P@users.noreply.github.com>
2021-05-04 14:53:44 +0530

    Update TesterForAmountTransferDetail.java

--- src/test/java/in/muthukumarip/TesterForAmountTransferDetail.java
+++ src/test/java/in/muthukumarip/TesterForAmountTransferDetail.java
@@ -2,49 +2,33 @@ package in.muthukumarip;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 public class TesterForAmountTransferDetail {
 
-	public static boolean validate(RecipientDetails recipient) {
-		boolean isValid = false;
-		ArrayList<String> errors = AmountTransferDetailsValidation.validateAmountTransferDetails(recipient);
-		if (errors.isEmpty()) {
-			System.out.println("Recipient Details are valid");
-			isValid = true;
-		} else {
-			System.out.println("Errors\n" + errors);
-			isValid = false;
-		}
-		return isValid;
-	}
-
 	// Test case for correct input
 	@Test
 	public void testCase1() {
-		RecipientDetails recipient = new RecipientDetails();
-		recipient.name = "MUTHU KUMARI";
-		recipient.accountNumber = 9876567876987654L;
-		recipient.bankName = "Indian Bank";
-		recipient.ifscCode = "IDIB000A089";
-		recipient.TransferAmount = 1000.0f;
-		boolean empty = validate(recipient);
-		assertEquals(true, empty);
+		RecipientDetails recipient1 = new RecipientDetails();
+		recipient1.name = "MUTHU KUMARI";
+		recipient1.accountNumber = 9876567876987654L;
+		recipient1.bankName = "Indian Bank";
+		recipient1.ifscCode = "IDIB000A089";
+		recipient1.TransferAmount = 1000.0f;
+		boolean empty = AmountTransferDetailsValidation.validate(recipient1);
+		assertEquals(true, empty); //expected,actual
 	}
 
 	// Test case for wrong input
 	@Test
 	public void testCase2() {
-		RecipientDetails recipient = new RecipientDetails();
-		recipient.name = " ";
-		recipient.accountNumber = 7223456798789123213L;
-		recipient.bankName = "             Indian Bank            ";
-		recipient.ifscCode = "IDIB000A089";
-		recipient.TransferAmount = 6000.0f;
-		boolean empty = validate(recipient);
-		assertEquals(false, empty);
+		RecipientDetails recipient2 = new RecipientDetails();
+		recipient2.name = " ";
+		recipient2.accountNumber = 7223456798789123213L;
+		recipient2.bankName = "             Indian Bank            ";
+		recipient2.ifscCode = "IDIB000A089";
+		recipient2.TransferAmount = 6000.0f;
+		boolean empty = AmountTransferDetailsValidation.validate(recipient2);
+		assertEquals(false, empty); //expected,actual
 	}
-
 }
