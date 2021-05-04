22d93e1d3ec14782ed0025aa459658f43781e8b2
MuthuKumari-P <82449715+MuthuKumari-P@users.noreply.github.com>
2021-05-04 14:57:38 +0530

    Update TesterForListOfBank.java

--- src/test/java/in/muthukumarip/TesterForListOfBank.java
+++ src/test/java/in/muthukumarip/TesterForListOfBank.java
@@ -1,10 +1,12 @@
 package in.muthukumarip;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TesterForListOfBank {
 	/**
-	 * TestCase for No bank name
+	 * TestCase for display Banks
 	 */
 	@Test
 	public void testCase1() {
@@ -15,49 +17,22 @@ public class TesterForListOfBank {
 	}
 
 	/**
-	 * Test case with one bank name
+	 * Test case with One Bank Name
 	 */
 	@Test
 	public void testCase2() {
-		String bankName = "Indian Bank";
-		OperationsOnBankList.addBankList(bankName);
-		OperationsOnBankList.displayBankList();
-		int noOfBanks = OperationsOnBankList.getNoOfBanks();
-		System.out.println(noOfBanks);
-
+		String bankName = "Indian Overseas Bank";
+		boolean bankNameValidation = CustomerBankDetailValidation.checkBankName(bankName);
+		assertEquals(true, bankNameValidation);
 	}
 
 	/**
-	 * test case with one bank name and with the condition
+	 * Test case with No Bank Name
 	 */
 	@Test
 	public void testCase3() {
-		String bankName = "Indian Overseas Bank";
-		boolean bankNameValidation = CustomerBankDetailValidation.nameValidation(bankName);
-		if (bankNameValidation) {
-			OperationsOnBankList.addBankList(bankName);
-			OperationsOnBankList.displayBankList();
-			int noOfBanks = OperationsOnBankList.getNoOfBanks();
-			System.out.println(noOfBanks);
-		} else {
-			System.out.println("Enter Valid Bank Name");
-		}
-	}
-
-	/**
-	 * Test case with No bank name and with condition
-	 */
-	@Test
-	public void testCase4() {
 		String bankName = " ";
-		boolean bankNameValidation = CustomerBankDetailValidation.nameValidation(bankName);
-		if (bankNameValidation) {
-			OperationsOnBankList.addBankList(bankName);
-			OperationsOnBankList.displayBankList();
-			int noOfBanks = OperationsOnBankList.getNoOfBanks();
-			System.out.println(noOfBanks);
-		} else {
-			System.out.println("Enter Valid Bank Name");
-		}
+		boolean bankNameValidation = CustomerBankDetailValidation.checkBankName(bankName);
+		assertEquals(false, bankNameValidation);
 	}
 }
